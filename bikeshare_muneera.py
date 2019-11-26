a66c813858f968c8c16d5de594bcc9b52e5fbc63
Muneera <m-qattan-1@hotmail.com>
2019-11-26 14:35:33 +0300

    Made changes

--- bikeshare_muneera.py
+++ bikeshare_muneera.py
@@ -16,7 +16,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s show some US bikeshare data!')
+    print('Hello! Let\'s explore  some US bikeshare data!')
 
     #Def function for user to input city
     while True:
